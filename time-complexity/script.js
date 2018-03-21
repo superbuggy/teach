c34fa598ceba1ae5c8fabf961d5c46e986345c8d
superbuggy <jpqreichard@gmail.com>
2018-03-21 19:36:33 -0400

    Dry up event handling code

--- time-complexity/script.js
+++ time-complexity/script.js
@@ -1,55 +1,55 @@
-document
-  .querySelector('#simple')
-  .addEventListener('click', () => {
-    const time = `Ran in ${measureTime(logArray).toFixed(3)} milliseconds`
-    consoleWrite(time)
-  })
-
-document
-  .querySelector('#quadratic')
-  .addEventListener('click', () => {
-    const time = `Ran in ${measureTime(twoNested).toFixed(3)} milliseconds`
-    consoleWrite(time)
-  })
-
-document
-  .querySelector('#cubic')
-  .addEventListener('click', () => {
-    const time = `Ran in ${measureTime(threeNested).toFixed(3)} milliseconds`
-    consoleWrite(time)
-  })
-
 const pageConsole = document.querySelector('code')
 const wholeNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const times = {
+  single: [],
+  quadratic: [],
+  cubic: []
+}
 
-function logArray () {
-  consoleWrite('Logging array...', true)
-  for (var i = 0; i < wholeNumbers.length; i++) {
-    consoleWrite(wholeNumbers[i])
-  }
-  consoleWrite('Done...', true)
+new Array(...document.querySelectorAll('button')).forEach(button => {
+  console.log(button.id)
+  button.addEventListener('click', _ => handleClick(button.id))
+})
+
+function handleClick (funcName) {
+  let func = eval(funcName)
+  pageConsole.innerHTML = ''
+  const milliseconds = measureTime(func).toFixed(3)
+  const time = `<span class="time">Ran in ${milliseconds} milliseconds</span>`
+  times[funcName].push(time)
+  consoleWrite(time)
+  pageConsole.scrollTo(0, pageConsole.scrollHeight)
 }
 
 function consoleWrite (message, newLine) {
   pageConsole.innerHTML += newLine ? `<br/> ${message} <br/>` : `${message}`
 }
 
-function twoNested () {
+function single () {
+  consoleWrite('in single, logging array...', true)
   for (var i = 0; i < wholeNumbers.length; i++) {
-    consoleWrite('in twoNested, iterating over: ' + wholeNumbers[i], true)
-    logArray()
+    consoleWrite(wholeNumbers[i])
+  }
+  consoleWrite('Done...', true)
+}
+
+function quadratic () {
+  for (var i = 0; i < wholeNumbers.length; i++) {
+    consoleWrite('in quadratic, outside of the innermost for-loop, iterating over: ' + wholeNumbers[i], true)
+    single()
   }
 }
 
-function threeNested () {
+function cubic () {
   for (var i = 0; i < wholeNumbers.length; i++) {
-    consoleWrite('in threeNested, iterating over: ' + wholeNumbers[i], true)
-    twoNested()
+    consoleWrite('in cubic, the outermost of 3 nested for-loops, iterating over: ' + wholeNumbers[i], true)
+    quadratic()
   }
 }
 
 // a way to measure time and execute the function
 function measureTime (func) {
+  pageConsole.innerHTML = ''
   let start = window.performance.now()
   console.time(func.name)
   func()
