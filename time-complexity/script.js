363e5c1d8cb9df41f2b3fe860d1886b6843b69d8
superbuggy <jpqreichard@gmail.com>
2018-03-21 18:53:50 -0400

    Update interface

--- time-complexity/script.js
+++ time-complexity/script.js
@@ -1,40 +1,60 @@
-document.querySelector('#simple').addEventListener('click', () => measureTime(logArray))
-document.querySelector('#quadratic').addEventListener('click', () => measureTime(twoNested))
-document.querySelector('#cubic').addEventListener('click', () => measureTime(threeNested))
-document.querySelector('#quartic').addEventListener('click', () => measureTime(fourNested))
-document.querySelector('code')
+document
+  .querySelector('#simple')
+  .addEventListener('click', () => {
+    const time = `Ran in ${measureTime(logArray).toFixed(3)} milliseconds`
+    consoleWrite(time)
+  })
+
+document
+  .querySelector('#quadratic')
+  .addEventListener('click', () => {
+    const time = `Ran in ${measureTime(twoNested).toFixed(3)} milliseconds`
+    consoleWrite(time)
+  })
+
+document
+  .querySelector('#cubic')
+  .addEventListener('click', () => {
+    const time = `Ran in ${measureTime(threeNested).toFixed(3)} milliseconds`
+    consoleWrite(time)
+  })
+
+const pageConsole = document.querySelector('code')
 const wholeNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function logArray () {
+  consoleWrite('Logging array...', true)
   for (var i = 0; i < wholeNumbers.length; i++) {
-    console.log(wholeNumbers[i])
+    consoleWrite(wholeNumbers[i])
   }
+  consoleWrite('Done...', true)
+}
+
+function consoleWrite (message, newLine) {
+  pageConsole.innerHTML += newLine ? `<br/> ${message} <br/>` : `${message}`
 }
 
 function twoNested () {
   for (var i = 0; i < wholeNumbers.length; i++) {
-    console.log('in twoNested, iterating over:', wholeNumbers[i])
+    consoleWrite('in twoNested, iterating over: ' + wholeNumbers[i], true)
     logArray()
   }
 }
 
 function threeNested () {
   for (var i = 0; i < wholeNumbers.length; i++) {
-    console.log('in threeNested, iterating over:', wholeNumbers[i])
+    consoleWrite('in threeNested, iterating over: ' + wholeNumbers[i], true)
     twoNested()
-  }
-}
-
-function fourNested () {
-  for (var i = 0; i < wholeNumbers.length; i++) {
-    console.log('in fourNested', wholeNumbers[i])
-    threeNested()
   }
 }
 
 // a way to measure time and execute the function
 function measureTime (func) {
+  let start = window.performance.now()
   console.time(func.name)
   func()
   console.timeEnd(func.name)
+  let end = window.performance.now()
+  console.log(start, end, end - start)
+  return end - start
 }
