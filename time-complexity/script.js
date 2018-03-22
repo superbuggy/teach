6a7f5825d3fe59437c78f32100b730767450b43f
superbuggy <jpqreichard@gmail.com>
2018-03-21 21:59:32 -0400

    Add average times display

--- time-complexity/script.js
+++ time-complexity/script.js
@@ -1,4 +1,5 @@
 const pageConsole = document.querySelector('code')
+const timesDisplay = document.querySelector('div.times')
 const wholeNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const times = {
   single: [],
@@ -15,10 +16,21 @@ function handleClick (funcName) {
   let func = eval(funcName)
   pageConsole.innerHTML = ''
   const milliseconds = measureTime(func).toFixed(3)
-  const time = `<span class="time">Ran in ${milliseconds} milliseconds</span>`
-  times[funcName].push(time)
-  consoleWrite(time)
+  const timeRanSpan = `<span class="time">Ran in ${milliseconds} milliseconds</span>`
+  updateTimes(funcName, milliseconds)
+  consoleWrite(timeRanSpan, true)
+  console.log(timeRanSpan, pageConsole)
   pageConsole.scrollTo(0, pageConsole.scrollHeight)
+}
+
+function updateTimes (funcName, time) {
+  times[funcName].push(time)
+  let mean = times[funcName]
+    .map(Number)
+    .reduce((sum, current) => sum + current, 0) / times[funcName].length
+  const functionTimeDisplay = document.querySelector(`p.${funcName}`)
+  console.log(functionTimeDisplay)
+  functionTimeDisplay.innerHTML = `${mean.toFixed(3)} milliseconds`
 }
 
 function consoleWrite (message, newLine) {
