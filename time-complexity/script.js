f3dffca0cae95757f1a7d05340de58682731d2eb
James Reichard <jpqreichard@gmail.com>
2018-06-20 14:48:49 -0400

    Hide scrollbar on load

--- time-complexity/script.js
+++ time-complexity/script.js
@@ -1,5 +1,4 @@
 const pageConsole = document.querySelector('code')
-const timesDisplay = document.querySelector('div.times')
 const wholeNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const times = {
   single: [],
@@ -7,22 +6,21 @@ const times = {
   cubic: []
 }
 
-new Array(...document.querySelectorAll('button')).forEach(button => {
+Array.from(document.querySelectorAll('button')).forEach(button => {
   button.addEventListener('click', _ => handleClick(button.id))
 })
 
-function handleClick (funcName) {
+function handleClick(funcName) {
   let func = window[funcName]
   pageConsole.innerHTML = ''
   const milliseconds = measureTime(func).toFixed(3)
   const timeRanSpan = `<span class="time">Ran in ${milliseconds} milliseconds</span>`
   updateTimes(funcName, milliseconds)
   consoleWrite(timeRanSpan, true)
-  console.log(timeRanSpan, pageConsole)
   pageConsole.scrollTo(0, pageConsole.scrollHeight)
 }
 
-function updateTimes (funcName, time) {
+function updateTimes(funcName, time) {
   times[funcName].push(time)
   let mean = times[funcName]
     .map(Number)
@@ -32,18 +30,18 @@ function updateTimes (funcName, time) {
   functionTimeDisplay.innerHTML = `${mean.toFixed(3)} milliseconds`
 }
 
-function consoleWrite (message, newLine) {
+function consoleWrite(message, newLine) {
   pageConsole.innerHTML += newLine ? `<br/> ${message} <br/>` : `${message}`
 }
 
-function single () {
+function single() {
   consoleWrite('Logging array in function <i>single</i>...', true)
   for (var i = 0; i < wholeNumbers.length; i++) {
     consoleWrite(`${wholeNumbers[i]} `)
   }
 }
 
-function quadratic () {
+function quadratic() {
   for (var i = 0; i < wholeNumbers.length; i++) {
     consoleWrite(`------------------------------`, true)
     consoleWrite(`Iterating over ${wholeNumbers[i]} in function <i>quadratic</i>, outside of the inner-most for-loop`, true)
@@ -51,7 +49,7 @@ function quadratic () {
   }
 }
 
-function cubic () {
+function cubic() {
   consoleWrite(`==============================`, true)
   for (var i = 0; i < wholeNumbers.length; i++) {
     consoleWrite(`Iterating over ${wholeNumbers[i]} in function <i>cubic</i>, the outermost of 3 nested for-loops`, true)
@@ -60,7 +58,7 @@ function cubic () {
 }
 
 // a way to measure time and execute the function
-function measureTime (func) {
+function measureTime(func) {
   let start = window.performance.now()
   console.time(func.name)
   func()
